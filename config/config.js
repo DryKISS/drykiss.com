e1952b5eacf4e3ee2345ab9bfdc3ce18261565b9
Ian Warner <ian.warner@drykiss.com>
2019-06-28 12:38:59 +0200

    updated config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ export const GOOGLE_ANALYTICS = 'UA-49976138-1'
 export const GOOGLE_ADSENSE = 'ca-pub-4390370568784157'
 export const GOOGLE_VERIFICATION = 'KV07s1r94b_CG89V9Tuh7a5FYEv4kG8-WbLMGLCOIjw'
 
+// Mailchimp
 // username : "drykiss"
 // dc       : "us7"
 // u        : "4862895059f8ff67d43123d97"
