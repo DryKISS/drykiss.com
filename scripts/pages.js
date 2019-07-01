d8b3a2880e2444f026fb006ad3e055552dce03bb
Ian Warner <ian.warner@drykiss.com>
2019-07-01 19:34:26 +0200

    updated pages

--- scripts/pages.js
+++ scripts/pages.js
@@ -7,11 +7,20 @@ module.exports = () => {
     '/': { page: '/' },
 
     '/404': { page: '/404' },
+
+    '/domains': { page: '/domains' },
+
     '/our-work': { page: '/our-work' },
+    '/what-we-do': { page: '/what-we-do' },
+
+    '/portfolio/facebook/brazil-soccer-name-generator': { page: '/portfolio/facebook/brazil-soccer-name-generator' },
+    '/portfolio/facebook/guinness-arthurs-day': { page: '/portfolio/facebook/guinness-arthurs-day' },
+    '/portfolio/facebook/walt-disney-prince-of-persia': { page: '/portfolio/facebook/walt-disney-prince-of-persia' },
+
     '/privacy-policy': { page: '/privacy-policy' },
-    '/site-map': { page: '/site-map' },
     '/terms-conditions': { page: '/terms-conditions' },
-    '/what-we-do': { page: '/what-we-do' }
+
+    '/site-map': { page: '/site-map' }
 
   })
 }
