b4afa3b830c158a5bdaddeb58c3373b815cd0bd3
xxinyiwho <xxinyiwho@gmail.com>
2020-01-27 23:02:51 +0100

    FEAT: team member added

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,16 +1,33 @@
 /**
- * Team
- */
+* Team
+*/
 
 // UI
-import { Card, CardBody, Column, Page, Row } from 'industry-ui'
+import { Card, Image, Column, Page, Row } from 'industry-ui'
+
+function teamMembers () {
+  return [
+    {
+      id: 'iwarner',
+      name: 'Ian Warner',
+      link: 'team/Ian-Warner',
+      image: '/static/freelancer/ian-warner/1.jpg'
+    },
+    {
+      id: 'kheruc',
+      name: 'Kristian Heruc',
+      link: 'team/Ian-Warner',
+      image: '/static/freelancer/ian-warner/1.jpg'
+    }
+  ]
+}
 
 export default () => {
   const meta = {
     description: `
-      DryKISS is a full service internet and mobile digital production house.
-      Our services span consulting, strategy; planning; development; testing
-      and analytics.
+    DryKISS is a full service internet and mobile digital production house.
+    Our services span consulting, strategy; planning; development; testing
+    and analytics.
     `,
     path: '/team',
     title: 'DryKISS develops hybrid mobile and responsive websites'
@@ -22,18 +39,19 @@ export default () => {
       <p>In DryKISS we offer a full service development and marketing solution, we are prepared to listen to your needs, craft your project and help boost your business. Our team is comprised of highly skilled professionals with over 20 years of experiences in web development and digital marketing, which secured the success of our collaboration.</p>
 
       <Row>
-        <Column lg={3}>
-          <Card
-            alt='Ian Warner'
-            bordered
-            image='/static/freelancer/ian-warner/1.jpg'
-            shadow
-            to='team/Ian-Warner'
-          >
-            <CardBody>Ian Warner</CardBody>
-          </Card>
-        </Column>
+        {teamMembers().map(member => (
+          <Column lg={3}>
+            <Card
+              center
+              footer={member.name}
+              to={{ href: member.link }}
+            >
+              <Image alt='' src={member.image} />
+            </Card>
+          </Column>
+        ))}
       </Row>
     </Page>
   )
 }
+
