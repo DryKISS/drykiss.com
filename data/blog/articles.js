d4fd350c2ec06298a3da1daf56bc3dcf606bb534
Ian Warner <ian.warner@drykiss.com>
2020-02-03 15:39:52 +0100

    fix: removed old blog - fixed hero size

--- data/blog/articles.js
+++ data/blog/articles.js
@@ -20,37 +20,5 @@ module.exports.Articles = [
     tags: ['static', 'browser', 'developer'],
     time: '12:00pm',
     title: 'Is learning the browser important for developers?'
-  },
-  {
-    author: 'Ian Warner',
-    category: 'Development',
-    data: '<p>Static Site development</p>',
-    date: '2019-07-16',
-    description: 'Static Site development',
-    excerpt: 'Static Site development',
-    heading: 'Static Site development',
-    hero: true,
-    published: true,
-    readtime: 2,
-    slug: 'static-site-development',
-    tags: ['static', 'jam'],
-    time: '12:00pm',
-    title: 'Static Site development'
-  },
-  {
-    author: 'Ian Warner',
-    category: 'Development',
-    data: '<p>Static Site development</p>',
-    date: '2019-07-16',
-    description: 'Static Site development',
-    excerpt: 'Static Site development',
-    heading: 'Static Site development',
-    hero: true,
-    published: true,
-    readtime: 2,
-    slug: 'static-site-development',
-    tags: ['12'],
-    time: '12:00pm',
-    title: 'Static Site development'
   }
 ]
