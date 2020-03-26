14fd7f68bb794b1fce3362adfcc2c387bed6febe
Naren <narayananbtech@gmail.com>
2020-03-26 21:09:03 +0530

    heading font family updated

--- config/theme.js
+++ config/theme.js
@@ -6,6 +6,28 @@ const TYPOGRAPHY = {
   font: 'JetBrains Mono'
 }
 
+export const HEADINGS = {
+  h1: {
+    fontFamily: TYPOGRAPHY.font
+  },
+  h2: {
+    fontFamily: TYPOGRAPHY.font
+  },
+  h3: {
+    fontFamily: TYPOGRAPHY.font
+  },
+  h4: {
+    fontFamily: TYPOGRAPHY.font
+  },
+  h5: {
+    fontFamily: TYPOGRAPHY.font
+  },
+  h6: {
+    fontFamily: TYPOGRAPHY.font
+  }
+}
+
 export const Theme = {
-  TYPOGRAPHY
+  TYPOGRAPHY,
+  HEADINGS
 }
