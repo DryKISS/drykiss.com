30fd7b6c1dd0479f0ff8c8db667d6f2d500c6076
Mi Zemkina <zemqee@gmail.com>
2020-04-27 10:40:37 +0600

    fix: add border component

---
+++ components/border/index.js
@@ -0,0 +1,97 @@
+/**
+ * Components — Border
+ */
+
+// React
+import { node, string } from 'prop-types'
+
+// UI
+import { MEDIA_QUERY } from 'industry-ui'
+
+// Style
+import styled, { css } from 'styled-components'
+import { mix, tint, transparentize } from 'polished'
+
+export const Border = ({ children, content, size, backgroundSide }) => {
+  return (
+    <StyledBorder size={size}>
+      <StyledBackground backgroundSide={backgroundSide}>{content || children}</StyledBackground>
+    </StyledBorder>
+  )
+}
+
+const StyledBorder = styled.div`
+  border: 10px solid transparent;
+  -moz-border-image: url(/static/border/border10.svg) 10;
+  -webkit-border-image: url(/static/border/border10.svg) 10;
+  -o-border-image: url(/static/border/border10.svg) 10;
+  border-image: url(/static/border/border10.svg) 10;
+
+  ${MEDIA_QUERY.tablet`
+    border: 18px solid transparent;
+    -moz-border-image: url(/static/border/border18.svg) 18;
+    -webkit-border-image: url(/static/border/border18.svg) 18;
+    -o-border-image: url(/static/border/border18.svg) 18;
+    border-image: url(/static/border/border18.svg) 18;
+  `}
+  ${MEDIA_QUERY.desktop`
+    border: 42px solid transparent;
+    -moz-border-image: url(/static/border/border42.svg) 42;
+    -webkit-border-image: url(/static/border/border42.svg) 42;
+    -o-border-image: url(/static/border/border42.svg) 42;
+    border-image: url(/static/border/border42.svg) 42;
+  `}
+`
+
+const StyledBackground = styled.div`
+  margin-left: -8px;
+  margin-bottom: -8px;
+  ${MEDIA_QUERY.tablet`
+    margin-left: -16px;
+    margin-bottom: -16px;
+  `}
+  ${MEDIA_QUERY.desktop`
+    margin-left: -40px;
+    margin-bottom: -40px;
+  `}
+
+  ${({ backgroundSide }) =>
+    backgroundSide === 'right' &&
+    css`
+      margin-left: 0;
+      margin-bottom: 0;
+      margin-right: -8px;
+      margin-top: -8px;
+      ${MEDIA_QUERY.tablet`
+        margin-left: 0;
+        margin-bottom: 0;
+        margin-right: -16px;
+        margin-top: -16px;
+      `}
+      ${MEDIA_QUERY.desktop`
+        margin-left: 0;
+        margin-bottom: 0;
+        margin-right: -40px;
+        margin-top: -40px;
+      `}
+    `}
+
+  background: ${({ theme }) =>
+    transparentize(
+      0.04,
+      mix(0.5, tint(0.04, theme.COLOUR.black), tint(0.96, theme.COLOUR.primary))
+    )};
+
+  padding: 2rem 0;
+`
+
+Border.protoTypes = {
+  children: node,
+  content: string,
+  size: string,
+  backgroundSide: string
+}
+
+Border.defaultProps = {
+  size: '10'
+}
