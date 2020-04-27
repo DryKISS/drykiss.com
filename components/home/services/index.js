bf30baaf66442e7689c44d26bdd9c14171cb8533
Mi Zemkina <zemqee@gmail.com>
2020-04-27 12:13:44 +0600

    fix: improved services

--- components/home/services/index.js
+++ components/home/services/index.js
@@ -4,67 +4,52 @@
 
 // UI
 import { Column, Container, Heading, Image, Row } from 'industry-ui'
-import { Text } from 'components'
-
-// Style
-import styled from 'styled-components'
+import { Space, Text } from 'components'
 
 export const Services = () => {
   return (
     <Container>
-      <StyledTitle content='Services' tag='h2' />
+      <Heading content='Services' tag='h2' />
 
-      <StyledSubTitle
+      <Heading
         content='We pride ourselves on offering a full-service, economic and future technology approach.'
         tag='h5'
       />
 
-      <StyledImage alt='Services' src='/static/services/services.jpg' title='Services' />
-
+      <Image alt='Services' src='/static/services/services.jpg' title='Services' />
+      <Space marginBottom='xl' />
       <Row>
         <Column md={12} lg={4}>
-          <StyledH6 content='User Experience' tag='h6' />
+          <Heading content='User Experience' tag='h6' />
 
           <Text>
             We help brands create valuable experiences across digital channels by focusing on the
             habits and needs of their audience.
           </Text>
+          <Space marginBottom='xl' />
         </Column>
 
         <Column md={6} lg={4}>
-          <StyledH6 content='Creativity' tag='h6' />
+          <Heading content='Creativity' tag='h6' />
 
           <Text>
             Focused creativity is the engine for all we do. Our cross-platform, and specifically
             mobile experience makes our approach always flexible, responsive and dynamic.
           </Text>
+          <Space marginBottom='xl' />
         </Column>
 
         <Column md={6} lg={4}>
-          <StyledH6 content='Development' tag='h6' />
+          <Heading content='Development' tag='h6' />
 
           <Text>
             By using the best and latest frameworks we are not re-inventing the wheel, but
             delivering projects based on known processes. This enables us to deliver cutting edge
             code quickly and within budget.
           </Text>
+          <Space marginBottom='xl' />
         </Column>
       </Row>
     </Container>
   )
 }
-
-const StyledTitle = styled(Heading)`
-  margin: 0 0 2rem 0;
-`
-
-const StyledSubTitle = styled(StyledTitle)``
-
-const StyledImage = styled(Image)`
-  display: block;
-  margin-bottom: 2rem;
-`
-
-const StyledH6 = styled(Heading)`
-  margin: 0 0 0.5rem 0;
-`
