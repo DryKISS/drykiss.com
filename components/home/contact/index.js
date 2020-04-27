1550ae654c5a26dad9a8af9bce3438169cbad728
Mi Zemkina <zemqee@gmail.com>
2020-04-27 12:08:53 +0600

    fix: improved contact

--- components/home/contact/index.js
+++ components/home/contact/index.js
@@ -3,7 +3,7 @@
  */
 
 // UI
-import { Container, Heading } from 'industry-ui'
+import { Container, Divider, Heading } from 'industry-ui'
 import { Text } from 'components'
 
 // Style
@@ -12,14 +12,16 @@ import styled from 'styled-components'
 export const Contact = () => {
   return (
     <Container>
-      <StyledSubTitle
+      <Divider size='lg' />
+
+      <StyledTitle
         content='If you have a great idea, now is the time to execute it. Contact us!'
         tag='h5'
       />
 
-      <Text size='xxxl'>+44 (0) 778 043 5329</Text>
+      <Text size='xxl'>+44 (0) 778 043 5329</Text>
 
-      <Text size='xxxl'>info@drykiss.com</Text>
+      <Text size='xxl'>info@drykiss.com</Text>
     </Container>
   )
 }
@@ -27,5 +29,3 @@ export const Contact = () => {
 const StyledTitle = styled(Heading)`
   margin: 0 0 2rem 0;
 `
-
-const StyledSubTitle = styled(StyledTitle)``
