58ed54c5677adbb46797a4d568bbd7c32490e192
zemqi <zemqee@gmail.com>
2020-04-28 11:03:04 +0600

    fix: deleted styled property

--- components/home/contact/index.js
+++ components/home/contact/index.js
@@ -4,28 +4,21 @@
 
 // UI
 import { Container, Divider, Heading } from 'industry-ui'
-import { Text } from 'components'
-
-// Style
-import styled from 'styled-components'
+import { Space, Text } from 'components'
 
 export const Contact = () => {
   return (
     <Container>
       <Divider size='lg' />
 
-      <StyledTitle
+      <Heading
         content='If you have a great idea, now is the time to execute it. Contact us!'
         tag='h5'
       />
+      <Space marginBottom='xl' />
 
       <Text size='xxl'>+44 (0) 778 043 5329</Text>
-
       <Text size='xxl'>info@drykiss.com</Text>
     </Container>
   )
 }
-
-const StyledTitle = styled(Heading)`
-  margin: 0 0 2rem 0;
-`
