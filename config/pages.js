b8c5b365a9258176994dd320dbe88edb8d928832
Majid Amiri <majid.drykiss1997@gmail.com>
2021-01-19 11:25:52 +0330

    remove extra pages config

--- config/pages.js
+++ config/pages.js
@@ -2,94 +2,50 @@
  * Pages for the site
  */
 
-const slugify = string => {
-  const slug = string
-    .toLowerCase()
-    .replace(/['&]/g, '')
-    .replace(/ +/g, ' ')
-    .replace(/ /g, '-')
-
-  return slug
-}
-
 module.exports = () => {
-  const ARTICLES = require('../data/blog/articles')
+  return Object.assign({
+    // Core
+    '/': { page: '/' },
+    '/404': { page: '/404' },
 
-  const articles = ARTICLES.Articles.reduce((pages, { category, slug }) => {
-    const catSlug = slugify(category)
+    // About
+    '/about': { page: '/about' },
 
-    return Object.assign({}, pages, {
-      [`/blog/${catSlug}/${slug}`]: {
-        page: '/blog/[categoryId]/[articleId]'
-      }
-    })
-  }, {})
+    // Blog
+    '/blog': { page: '/blog' },
 
-  return Object.assign(
-    {
-      ...articles
+    // Careers
+    '/careers': { page: '/careers' },
+    '/careers/react': { page: '/careers/react' },
+    '/careers/react-intern': { page: '/careers/react-intern' },
+    '/careers/react-junior': { page: '/careers/react-junior' },
+
+    // Contact
+    '/contact': { page: '/contact' },
+
+    // Our Work
+    '/our-work': { page: '/our-work' },
+
+    // Policies
+    '/policies/privacy-policy': { page: '/policies/privacy-policy' },
+    '/policies/terms-conditions': { page: '/policies/terms-conditions' },
+    '/policies/site-map': { page: '/policies/site-map' },
+
+    // Portfolio
+    '/portfolio/facebook/brazil-soccer-name-generator': {
+      page: '/portfolio/facebook/brazil-soccer-name-generator'
     },
-    {
-      // Core
-      '/': { page: '/' },
-      '/404': { page: '/404' },
+    '/portfolio/facebook/guinness-arthurs-day': {
+      page: '/portfolio/facebook/guinness-arthurs-day'
+    },
+    '/portfolio/facebook/walt-disney-prince-of-persia': {
+      page: '/portfolio/facebook/walt-disney-prince-of-persia'
+    },
 
-      // About
-      '/about': { page: '/about' },
+    // Team
+    '/team': { page: '/team' },
 
-      // Blog
-      '/blog': { page: '/blog' },
-
-      // Careers
-      '/careers': { page: '/careers' },
-      '/careers/react': { page: '/careers/react' },
-      '/careers/react-intern': { page: '/careers/react-intern' },
-      '/careers/react-junior': { page: '/careers/react-junior' },
-
-      // Contact
-      '/contact': { page: '/contact' },
-
-      // Domain
-      '/domains': { page: '/domains' },
-
-      // Guides
-      '/guides': { page: '/guides' },
-      '/guides/continuous-intergration/git': { page: '/guides/continuous-intergration/git' },
-      '/guides/continuous-intergration/github': { page: '/guides/continuous-intergration/github' },
-      '/guides/linting': { page: '/guides/linting' },
-      '/guides/mysql': { page: '/guides/mysql' },
-      '/guides/nextjs/anatomy': { page: '/guides/nextjs/anatomy' },
-
-      // Our Work
-      '/our-work': { page: '/our-work' },
-
-      // Policies
-      '/policies/privacy-policy': { page: '/policies/privacy-policy' },
-      '/policies/terms-conditions': { page: '/policies/terms-conditions' },
-      '/policies/site-map': { page: '/policies/site-map' },
-
-      // Portfolio
-      '/portfolio/facebook/brazil-soccer-name-generator': {
-        page: '/portfolio/facebook/brazil-soccer-name-generator'
-      },
-      '/portfolio/facebook/guinness-arthurs-day': {
-        page: '/portfolio/facebook/guinness-arthurs-day'
-      },
-      '/portfolio/facebook/walt-disney-prince-of-persia': {
-        page: '/portfolio/facebook/walt-disney-prince-of-persia'
-      },
-
-      // Team
-      '/team': { page: '/team' },
-
-      // What We Do
-      '/what-we-do': { page: '/what-we-do' },
-      '/what-we-do/services/digital-marketing': { page: '/what-we-do/services/digital-marketing' },
-      '/what-we-do/services/mobile': { page: '/what-we-do/services/mobile' },
-      '/what-we-do/services/pricing': { page: '/what-we-do/services/pricing' },
-      '/what-we-do/services/prototyping': { page: '/what-we-do/services/prototyping' },
-      '/what-we-do/services/responsive': { page: '/what-we-do/services/responsive' },
-      '/what-we-do/services/seo': { page: '/what-we-do/services/seo' }
-    }
-  )
+    // What We Do
+    '/what-we-do': { page: '/what-we-do' }
+  })
 }
