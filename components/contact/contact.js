3282931357ec394ef09ddfed63a55f10e9671cc8
Majid Amiri <majid.drykiss1997@gmail.com>
2021-02-09 00:26:22 +0330

    Contact content

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -11,14 +11,7 @@ import { ContactForm } from './form'
 export const Contact = () => {
   return (
     <Container>
-      <Paragraph
-        content={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-         in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-          anim id est laborum.`}
-      />
+      <Paragraph content='Leave your information and we will schedule a call with you.' />
 
       <Space marginBottom='xxxl' />
 
