6668755d311e60aadf17c3c40eb15c06b8116b14
mohsen <mhsnprvr4@gmail.com>
2021-06-11 12:49:59 +0430

    fix(App):finetuning

--- layouts/index.js
+++ layouts/index.js
@@ -17,7 +17,7 @@ import { Copyright } from '../config/copyright'
 import { Footer } from '../config/footer'
 import { Navigation } from '../config/navigation'
 import GlobalStyles from '../styles/globalStyles'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 const Layout = ({ children }) => {
   return (
@@ -33,15 +33,15 @@ const Layout = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
       </Head>
 
-      {/*<StyledMain>{children}</StyledMain>*/}
-			{children}
-			<GlobalStyles />
+      {/* <StyledMain>{children}</StyledMain> */}
+      {children}
+      <GlobalStyles />
     </Bootstrap>
   )
 }
-//const StyledMain=styled.main`
-//margin-top: -62px;
-//`
+// const StyledMain=styled.main`
+// margin-top: -62px;
+// `
 Layout.propTypes = {
   children: node.isRequired
 }
